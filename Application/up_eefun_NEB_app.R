123cae173d25f6c39774eaf9ba424b805894fbd5
Bryan Blette <bletteb18@gmail.com>
2019-02-13 13:45:45 -0500

    Change Ytau to Y_tau

--- Application/up_eefun_NEB_app.R
+++ Application/up_eefun_NEB_app.R
@@ -3,17 +3,17 @@
 up_eefun_NEB_cc <- function(data, beta0range, beta1range, contrast) {
   
   function(theta) {
-    with(data, c( (1-Ytau)*Z*1*(!is.na(S_star) & S_star==0)*(Y-theta[1])*(1/theta[13]*(1-Y)*R+Y),			                        # risk_1_00 = theta[1]
-                  (1-Ytau)*Z*1*(!is.na(S_star) & S_star==1)*(Y-theta[2])*(1/theta[13]*(1-Y)*R+Y),				                      # risk_1_10 = theta[2]
+    with(data, c( (1-Y_tau)*Z*1*(!is.na(S_star) & S_star==0)*(Y-theta[1])*(1/theta[13]*(1-Y)*R+Y),			                        # risk_1_00 = theta[1]
+                  (1-Y_tau)*Z*1*(!is.na(S_star) & S_star==1)*(Y-theta[2])*(1/theta[13]*(1-Y)*R+Y),				                      # risk_1_10 = theta[2]
                   
-                  Z*(1-Ytau-theta[3]),														                                                            # P(Ytau(1) = 0) = theta[3]
-                  (1-Z)*(1-Ytau-theta[4]),													                                                          # P(Ytau(0) = 0) = theta[4]
-                  theta[5] - theta[3]/theta[4],													                                                      # P(Ytau(1) = 0 | Ytau(0) = 0) = theta[5]
-                  (1-Z)*(1-Ytau)*(Y-theta[6]),													                                                      # P(Y(0) = 1 | Ytau(0) = 0) = theta[6]
+                  Z*(1-Y_tau-theta[3]),														                                                            # P(Y_tau(1) = 0) = theta[3]
+                  (1-Z)*(1-Y_tau-theta[4]),													                                                          # P(Y_tau(0) = 0) = theta[4]
+                  theta[5] - theta[3]/theta[4],													                                                      # P(Y_tau(1) = 0 | Y_tau(0) = 0) = theta[5]
+                  (1-Z)*(1-Y_tau)*(Y-theta[6]),													                                                      # P(Y(0) = 1 | Y_tau(0) = 0) = theta[6]
                   theta[6] - theta[7]*theta[5] - theta[8]*(1-theta[5]),									                                      # risk_0 = theta[7]
                   exp(min(beta0range))*theta[8]/(1-theta[8]) - theta[7]/(1-theta[7]),						                              # risk_new = theta[8]
                   
-                  (1-Ytau)*Z*(1*(!is.na(S_star) & S_star==1)-theta[9])*(1/theta[13]*(1-Y)*R+Y),			                        	# p_10 = theta[9] 
+                  (1-Y_tau)*Z*(1*(!is.na(S_star) & S_star==1)-theta[9])*(1/theta[13]*(1-Y)*R+Y),			                        	# p_10 = theta[9] 
                   theta[7] - theta[10]*(1-theta[9]) - theta[11]*theta[9],								                                      # risk_0_00 = theta[10]
                   exp(max(beta1range))*theta[11]/(1-theta[11]) - theta[10]/(1-theta[10]),						                          # risk_0_10 = theta[11]
                   
