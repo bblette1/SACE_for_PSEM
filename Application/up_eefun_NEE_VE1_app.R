4acd78133a06b1b2a87f301aa184e33cbf9683ab
Bryan Blette <bletteb18@gmail.com>
2019-02-13 21:06:59 -0500

    Eliminate vestigial functions

--- Application/up_eefun_NEE_VE1_app.R
+++ Application/up_eefun_NEE_VE1_app.R
@@ -1,69 +1,16 @@
-ScenarioB_up_eefun_min <- function(data, beta0range, contrast) {
+up_eefun_NEE_VE1_cc <- function(data, beta0range, contrast) {
   
   function(theta) {
-    with(data, c( (1-Ytau)*(1-Z)*(Y-theta[1]),						# risk_0 = theta[1]
-                  (1-Ytau)*S_star*Z*(Y-theta[2]),							# risk_1_10 = theta[2]
-                  (1-Ytau)*Z*(1-S_star-theta[3]),						# p_00 = theta[3]
-                  Z*(1-Ytau)*(1-S_star)*(Y-theta[4]), 					# risk_1_00 = theta[4]
-                  theta[1] - theta[5]*theta[3] - theta[6]*(1-theta[3]),			# risk_0_00 = theta[5] 
-                  exp(min(beta0range))*theta[6]/(1-theta[6]) - theta[5]/(1-theta[5]), # risk_0_10 = theta[6]
+    with(data, c( (1-Ytau)*(1-Z)*(Y-theta[1]),						                              # risk_0 = theta[1]
+                  (1-Ytau)*S_star*Z*(1/theta[8]*(1-Y)*R+Y)*(Y-theta[2]),							  # risk_1_10 = theta[2]
+                  (1-Ytau)*Z*(1/theta[8]*(1-Y)*R+Y)*(1-S_star-theta[3]),		            # p_00 = theta[3]
+                  Z*(1-Ytau)*(1-S_star)*(1/theta[8]*(1-Y)*R+Y)*(Y-theta[4]), 		        # risk_1_00 = theta[4]
+                  theta[1] - theta[5]*theta[3] - theta[6]*(1-theta[3]),			            # risk_0_00 = theta[5] 
+                  exp(max(beta0range))*theta[6]/(1-theta[6]) - theta[5]/(1-theta[5]),   # risk_0_10 = theta[6]
                   ( theta[7] - (1- theta[2]/theta[6]) )*(contrast == "VE") +
                   ( theta[7] - (theta[2] - theta[6]) )*(contrast == "Difference") +
-                  ( theta[7] - (log(theta[2]) - log(theta[6])) )*(contrast == "logRR")
-    ))
-  }
-  
-}
-
-ScenarioB_up_eefun_max <- function(data, beta0range, contrast) {
-  
-  function(theta) {
-    with(data, c( (1-Ytau)*(1-Z)*(Y-theta[1]),						# risk_0 = theta[1]
-                  (1-Ytau)*S_star*Z*(Y-theta[2]),							# risk_1_10 = theta[2]
-                  (1-Ytau)*Z*(1-S_star-theta[3]),						# p_00 = theta[3]
-                  Z*(1-Ytau)*(1-S_star)*(Y-theta[4]), 					# risk_1_00 = theta[4]
-                  theta[1] - theta[5]*theta[3] - theta[6]*(1-theta[3]),			# risk_0_00 = theta[5] 
-                  exp(max(beta0range))*theta[6]/(1-theta[6]) - theta[5]/(1-theta[5]), # risk_0_10 = theta[6]
-                  ( theta[7] - (1- theta[2]/theta[6]) )*(contrast == "VE") +
-                  ( theta[7] - (theta[2] - theta[6]) )*(contrast == "Difference") +
-                  ( theta[7] - (log(theta[2]) - log(theta[6])) )*(contrast == "logRR")
-    ))
-  }
-  
-}
-
-
-ScenarioB_up_eefun_cc_min <- function(data, beta0range, contrast) {
-  
-  function(theta) {
-    with(data, c( (1-Ytau)*(1-Z)*(Y-theta[1]),						# risk_0 = theta[1]
-                  (1-Ytau)*S_star*Z*(1/theta[8]*(1-Y)*R+Y)*(Y-theta[2]),							# risk_1_10 = theta[2]
-                  (1-Ytau)*Z*(1/theta[8]*(1-Y)*R+Y)*(1-S_star-theta[3]),		# p_00 = theta[3]
-                  Z*(1-Ytau)*(1-S_star)*(1/theta[8]*(1-Y)*R+Y)*(Y-theta[4]), 		# risk_1_00 = theta[4]
-                  theta[1] - theta[5]*theta[3] - theta[6]*(1-theta[3]),			# risk_0_00 = theta[5] 
-                  exp(min(beta0range))*theta[6]/(1-theta[6]) - theta[5]/(1-theta[5]), # risk_0_10 = theta[6]
-                  ( theta[7] - (1- theta[2]/theta[6]) )*(contrast == "VE") +
-                  ( theta[7] - (theta[2] - theta[6]) )*(contrast == "Difference") +
-                  ( theta[7] - (log(theta[2]) - log(theta[6])) )*(contrast == "logRR"),
-                  (1-Y)*(R-theta[8])
-    ))
-  }
-  
-}
-
-ScenarioB_up_eefun_cc_max <- function(data, beta0range, contrast) {
-  
-  function(theta) {
-    with(data, c( (1-Ytau)*(1-Z)*(Y-theta[1]),						# risk_0 = theta[1]
-                  (1-Ytau)*S_star*Z*(1/theta[8]*(1-Y)*R+Y)*(Y-theta[2]),							# risk_1_10 = theta[2]
-                  (1-Ytau)*Z*(1/theta[8]*(1-Y)*R+Y)*(1-S_star-theta[3]),		# p_00 = theta[3]
-                  Z*(1-Ytau)*(1-S_star)*(1/theta[8]*(1-Y)*R+Y)*(Y-theta[4]), 		# risk_1_00 = theta[4]
-                  theta[1] - theta[5]*theta[3] - theta[6]*(1-theta[3]),			# risk_0_00 = theta[5] 
-                  exp(max(beta0range))*theta[6]/(1-theta[6]) - theta[5]/(1-theta[5]), # risk_0_10 = theta[6]
-                  ( theta[7] - (1- theta[2]/theta[6]) )*(contrast == "VE") +
-                  ( theta[7] - (theta[2] - theta[6]) )*(contrast == "Difference") +
-                  ( theta[7] - (log(theta[2]) - log(theta[6])) )*(contrast == "logRR"),
-                  (1-Y)*(R-theta[8])
+                  ( theta[7] - (log(theta[2]) - log(theta[6])) )*(contrast == "logRR"), # CEP(1,0) - CEP(0,0) = theta[7]
+                  (1-Y)*(R-theta[8])                                                    # Pi = theta[8]
     ))
   }
   
